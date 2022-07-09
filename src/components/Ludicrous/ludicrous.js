e46bfc79f0a190f245a82601cdb7debfca22a74f
Kyle Patrick Bader <kbader91@gmail.com>
2022-07-09 16:32:27 -0700

    ludicrous section fully uploaded

--- src/components/Ludicrous/ludicrous.js
+++ src/components/Ludicrous/ludicrous.js
@@ -1,16 +1,53 @@
-import { Card, Container} from 'react-bootstrap';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import kickCan from "../../assets/images/kick-the-can.jpg";
+import tableTop from "../../assets/images/table-top-for-2.jpg";
+import cardGame from "../../assets/images/card-game_v1.jpg";
+import twoRighties from "../../assets/images/2-righties.jpg";
+import partyRocks from "../../assets/images/party-on-the-rocks.jpg";
+import dinnerParty from "../../assets/images/dinner-party.jpg";
+import dancingOnBarrels from "../../assets/images/dancing-on-barrels.jpg";
+import twoAssholes from "../../assets/images/2_assholes.jpg";
+import cigarFlick from "../../assets/images/burn_and_booze.jpg";
 import "./ludicrous.css";
 
 function Ludicrous() {
 
     return (
         <section id="ludicrousProjects">
-            <div id="kickCanIMG">
+            <div id="partyOnTheRocksIMG" className="piece">
+                <img src={partyRocks} alt="Portfolio piece"/>
+                <p>Digital collage, 8 x 12" (print), 2021</p>
+            </div>
+            <div id="kickCanIMG" className="piece">
                 <img src={kickCan} alt="Portfolio piece"/>
-                <p>Untitled, Print on paper, 8 x 12", 2021</p>
+                <p>Digital collage, 8 x 12" (print), 2021</p>
+            </div>
+            <div id="tableTopIMG" className="piece">
+                <img src={tableTop} alt="Portfolio piece"/>
+                <p>Digital collage, 18 x 12" (print), 2021</p>
+            </div>
+            <div id="cardGameIMG" className="piece">
+                <img src={cardGame} alt="Portfolio piece"/>
+                <p>Digital collage, 8 x 12" (print), 2021</p>
+            </div>
+            <div id="twoRightiesIMG" className="piece">
+                <img src={twoRighties} alt="Portfolio piece"/>
+                <p>Digital collage, 8 x 12" (print), 2021</p>
+            </div>
+            <div id="dinnerPartyIMG" className="piece">
+                <img src={dinnerParty} alt="Portfolio piece"/>
+                <p>Digital collage, 8 x 12" (print), 2021</p>
+            </div>
+            <div id="dancingOnBarrelsIMG" className="piece">
+                <img src={dancingOnBarrels} alt="Portfolio piece"/>
+                <p>Digital collage, 18 x 12" (print), 2021</p>
+            </div>
+            <div id="twoAssholesIMG" className="piece">
+                <img src={twoAssholes} alt="Portfolio piece"/>
+                <p>Digital collage, 8 x 12" (print), 2021</p>
+            </div>
+            <div id="cigarFlickIMG" className="piece">
+                <img src={cigarFlick} alt="Portfolio piece"/>
+                <p>Digital collage, 8 x 12" (print), 2021</p>
             </div>
         </section>
     )

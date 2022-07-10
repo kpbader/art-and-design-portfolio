1a61496352036d26ebf524e3420990a67e76b7e3
Kyle Patrick Bader <kbader91@gmail.com>
2022-07-09 18:48:15 -0700

    painting and mixed media section started

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Header from "./components/Header/header";
 import Home from "./components/Home/home";
 import Portfolio from "./components/Portfolio/portfolio";
 import Ludicrous from "./components/Ludicrous/ludicrous";
+import PaintMix from "./components/Paint-Mix/paint-mix";
 import Contact from "./components/Contact/contact";
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/Portfolio" element={<Portfolio />} />
         <Route path="/Ludicrous" element={<Ludicrous />} />
+        <Route path="/Paintingandmixedmedia" element={<PaintMix />} />
         <Route path="/Contact" element={<Contact />} />
       </Routes>
     </Router>
---
+++ src/components/Paint-Mix/paint-mix.js
@@ -0,0 +1,50 @@
+import greatTrain from "../../assets/images/greattrainfinal1.jpeg";
+import camAndVon from "../../assets/images/CamandVon1.jpg";
+import kPerry from "../../assets/images/KPerry1.jpg";
+import horseMan from "../../assets/images/Frenchhorseman_Retouched.jpg";
+import kUpton from "../../assets/images/KU1.jpg";
+import flagCrew from "../../assets/images/flag-crew.jpg";
+// import gWash from "../../assets/images/GW.jpg";
+
+import "./paint-mix.css";
+
+function Paint() {
+
+    return (
+        <section id="paintProjects">
+            <div id="greatTrainIMG" className="piece">
+                <img src={greatTrain} alt="Portfolio piece"/>
+                <p>Oil on masonite, 8 x 10", 2015</p>
+            </div>
+            <div id="camAndVonIMG" className="piece">
+                <img src={camAndVon} alt="Portfolio piece"/>
+                <p>Mixed media on panel, 48 x 48", 2017</p>
+            </div>
+            <div id="flagCrewIMG" className="piece">
+                <img src={flagCrew} alt="Portfolio piece"/>
+                <p>Mixed media on panel, 48 x 48", 2017</p>
+            </div>
+            <div id="kPerryIMG" className="piece">
+                <img src={kPerry} alt="Portfolio piece"/>
+                <p>Mixed media on panel, 48 x 48", 2017</p>
+            </div>
+            <div id="kUptonIMG" className="piece">
+                <img src={kUpton} alt="Portfolio piece"/>
+                <p>Mixed media on panel, 48 x 48", 2017</p>
+            </div>
+
+            <div id="horseIMG" className="piece">
+                <img src={horseMan} alt="Portfolio piece"/>
+                <p>Mixed media on panel, 48 x 48", 2017</p>
+            </div>
+            {/* <div id="gWashIMG" className="piece">
+                <img src={gWash} alt="Portfolio piece"/>
+                <p>Mixed media on panel, 48 x 48", 2017</p>
+            </div> */}
+
+        </section>
+    )
+
+}
+
+export default Paint;

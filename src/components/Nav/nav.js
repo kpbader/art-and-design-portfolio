408a64d31ddcff77c03692def8162ea021b53e9d
Kyle Patrick Bader <kbader91@gmail.com>
2022-12-22 10:19:35 -0800

    handleClose added in for nav tabs

--- src/components/Nav/nav.js
+++ src/components/Nav/nav.js
@@ -6,13 +6,10 @@ import { Button, Offcanvas } from 'react-bootstrap'
 
 function Nav() {
 
-    // const location = useLocation()
-
     // offCanvas 
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
-    // const onClick = () => setShow(true);
 
     return (
         <>
@@ -28,15 +25,15 @@ function Nav() {
                     <section id="nav-tabs">
                         <ul className="tabpanel">
 
-                            <Link to="/art-and-design-portfolio" className="tab" >Home</Link>
-                            <Link to="/Portfolio" className="tab" >Portfolio</Link>
-                            <a href="https://www.etsy.com/shop/kpbaderart?ref=shop_sugg" target="_blank" rel="noreferrer">
+                            <Link to="/art-and-design-portfolio" className="tab" onClick={handleClose}>Home</Link>
+                            <Link to="/Portfolio" className="tab" onClick={handleClose}>Portfolio</Link>
+                            <a href="https://www.etsy.com/shop/kpbaderart?ref=shop_sugg" target="_blank" rel="noreferrer" onClick={handleClose}>
                                 Shop
                             </a>
-                            <a href="https://drive.google.com/file/d/1TLtqU525SZYZX6LOYLrD6Caq5_iOcl27/view?usp=sharing" target="_blank" rel="noreferrer">
+                            <a href="https://drive.google.com/file/d/1TLtqU525SZYZX6LOYLrD6Caq5_iOcl27/view?usp=sharing" target="_blank" rel="noreferrer" onClick={handleClose}>
                                 Resume
                             </a>
-                            <Link to="/Contact" className="tab" >Contact</Link>
+                            <Link to="/Contact" className="tab" onClick={handleClose}>Contact</Link>
 
                         </ul>
                     </section>
